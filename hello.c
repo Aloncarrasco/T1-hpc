f8bbab5ee83a5b199b018779017693dbd85012f6
Alonso_Carrasco2000 <alonso.carrasco@edutecno.com>
2023-10-29 22:17:48 -0300

    cahnge

---
+++ hello.c
@@ -0,0 +1,27 @@
+/*
+ * Simplified simulation of fire extinguishing
+ *
+ * Computacion Paralela, Grado en Informatica (Universidad de Valladolid)
+ * 2018/2019
+ *
+ * v1.4
+ *
+ * Sequential reference code.
+ *
+ * (c) 2019 Arturo Gonzalez Escribano
+ */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<float.h>
+#include<sys/time.h>
+
+/*
+ * MAIN PROGRAM
+ */
+int main(int argc, char *argv[]) {
+	
+	printf("Hello world\n");
+	return 0;
+}
