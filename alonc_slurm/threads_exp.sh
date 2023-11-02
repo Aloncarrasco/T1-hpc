954f99824c30427d3746a2a8085e3d19d94fcc38
Alonso_Carrasco2000 <alonso.carrasco@edutecno.com>
2023-11-01 21:46:57 -0300

    change on slurm

--- alonc_slurm/threads_exp.sh
+++ alonc_slurm/threads_exp.sh
@@ -23,28 +23,21 @@ echo 'Compiling'
 gcc -fopenmp -lm -O3 '4_2_2_parallel.c' -lm -O3 -o '4_2_2_parallel.bin'
 
 #SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
 time ./4_2_2_parallel.bin -f test_files/test3
 
-#SBATCH --cpus-per-task=2
+#SBATCH --ntasks-per-node=2
 time ./4_2_2_parallel.bin -f test_files/test3
 
-#SBATCH --cpus-per-task=3
+#SBATCH --ntasks-per-node=3
 time ./4_2_2_parallel.bin -f test_files/test3
 
-#SBATCH --cpus-per-task=4
+#SBATCH --ntasks-per-node=4  
 time ./4_2_2_parallel.bin -f test_files/test3
 
-#SBATCH --cpus-per-task=5
+#SBATCH --ntasks-per-node=5  
 time ./4_2_2_parallel.bin -f test_files/test3
 
-#SBATCH --cpus-per-task=6
-time ./4_2_2_parallel.bin -f test_files/test3
-
-#SBATCH --cpus-per-task=7
-time ./4_2_2_parallel.bin -f test_files/test3
- 
-#SBATCH --cpus-per-task=8
-time ./4_2_2_parallel.bin -f test_files/test3
 
 
 sleep 10
