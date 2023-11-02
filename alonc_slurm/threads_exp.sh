1488243106a25ff8c7bc3dd43aa196ae294abfd0
Alonso_Carrasco2000 <alonso.carrasco@edutecno.com>
2023-11-01 22:03:14 -0300

    new slurm

--- alonc_slurm/threads_exp.sh
+++ alonc_slurm/threads_exp.sh
@@ -7,8 +7,10 @@
 #SBATCH --mail-type=END,FAIL         # notificacion cuando el trabajo termine o fa$
 #SBATCH --mail-user=cristian.carrasco@uc.cl       # mail donde mandar las notifica$
 #SBATCH --workdir=/home/cristian.carrasco/T1-hpc         # direccion del d$
-
+#SBATCH --nodes 1                    # numero de nodos a usar
+#SBATCH --ntasks-per-node=4        # numero de trabajos (procesos) por nodo
 #SBATCH --cpus-per-task=8            # numero de cpus (threads) por trabajo (proceso)
+
 
 function ends_with_c {
     file="$1"
@@ -22,21 +24,20 @@ function ends_with_c {
 echo 'Compiling'
 gcc -fopenmp -lm -O3 '4_2_2_parallel.c' -lm -O3 -o '4_2_2_parallel.bin'
 
-#SBATCH --cpus-per-task=1
-#SBATCH --ntasks-per-node=1
-time ./4_2_2_parallel.bin -f test_files/test3
+export OMP_NUM_THREADS=1
+time ./4_2_2_parallel.bin -f test_files/test4
 
-#SBATCH --ntasks-per-node=2
-time ./4_2_2_parallel.bin -f test_files/test3
+export OMP_NUM_THREADS=2
+time ./4_2_2_parallel.bin -f test_files/test4
 
-#SBATCH --ntasks-per-node=3
-time ./4_2_2_parallel.bin -f test_files/test3
+export OMP_NUM_THREADS=3
+time ./4_2_2_parallel.bin -f test_files/test4
 
-#SBATCH --ntasks-per-node=4  
-time ./4_2_2_parallel.bin -f test_files/test3
+ export OMP_NUM_THREADS=4
+time ./4_2_2_parallel.bin -f test_files/test4
 
-#SBATCH --ntasks-per-node=5  
-time ./4_2_2_parallel.bin -f test_files/test3
+ export OMP_NUM_THREADS=5
+time ./4_2_2_parallel.bin -f test_files/test4
 
 
 
