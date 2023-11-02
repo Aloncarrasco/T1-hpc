9aae5bd52fa04e4752a0f3b2028575ac4a5b6fb8
Alonso_Carrasco2000 <alonso.carrasco@edutecno.com>
2023-11-02 18:59:07 -0300

    new slurm

--- alonc_slurm/AB5_config_6.sh
+++ alonc_slurm/AB5_config_6.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=hpc_t1_klempau       # nombre del trabajo
 #SBATCH -p ialab-high
 #SBATCH -t 0-2:00                    # tiempo maximo en el cluster (D-HH:MM)
-#SBATCH -o 16_task_1_cpu.out                 # STDOUT (A = )
+#SBATCH -o SLURM6.out                 # STDOUT (A = )
 #SBATCH -e c_job.err                 # STDERR
 #SBATCH --mail-type=END,FAIL         # notificacion cuando el trabajo termine o fa$
 #SBATCH --mail-user=cristian.carrasco@uc.cl       # mail donde mandar las notifica$
@@ -10,7 +10,6 @@
 #SBATCH --nodes 1                    # numero de nodos a usar
 #SBATCH --ntasks-per-node=16        # numero de trabajos (procesos) por nodo
 #SBATCH --cpus-per-task=1          # numero de cpus (threads) por trabajo (proceso)
-
 
 
 echo '--------------------------- Test 4 --------------------------------'
@@ -26,7 +25,7 @@ echo 'test 3'
 gcc -fopenmp -lm -O3 $file -lm -O3 -o $file_bin
 
 echo 'no exports'
-time $file_bin -f test_files/test3
+srun $file_bin -f test_files/test3
 
 
 sleep 5
