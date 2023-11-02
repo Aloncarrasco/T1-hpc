4dfbb750ee565101b2a55591f573b5974294226b
Alonso_Carrasco2000 <alonso.carrasco@edutecno.com>
2023-11-01 21:36:24 -0300

    slurm cahnges

--- alonc_slurm/threads_exp.sh
+++ alonc_slurm/threads_exp.sh
@@ -7,7 +7,8 @@
 #SBATCH --mail-type=END,FAIL         # notificacion cuando el trabajo termine o fa$
 #SBATCH --mail-user=cristian.carrasco@uc.cl       # mail donde mandar las notifica$
 #SBATCH --workdir=/home/cristian.carrasco/T1-hpc         # direccion del d$
-#
+
+#SBATCH --cpus-per-task=8            # numero de cpus (threads) por trabajo (proceso)
 
 function ends_with_c {
     file="$1"
@@ -21,14 +22,14 @@ function ends_with_c {
 echo 'Compiling'
 gcc -fopenmp -lm -O3 '4_2_2_parallel.c' -lm -O3 -o '4_2_2_parallel.bin'
 
-#SBATCH --nodes 1                    # numero de nodos a usar
-#SBATCH --cpus-per-task=32           # numero de cpus (threads) por trabajo (proceso)
+#SBATCH --cpus-per-task=1
+time ./4_2_2_parallel.bin -f test_files/test3
 
-# export OMP_NUM_THREADS=16
-time ./4_2_2_parallel.bin -f test_files/test2
+#SBATCH --cpus-per-task=2
+time ./4_2_2_parallel.bin -f test_files/test3
 
-# export OMP_NUM_THREADS=32
-time ./4_2_2_parallel.bin -f test_files/test2
+#SBATCH --cpus-per-task=3
+time ./4_2_2_parallel.bin -f test_files/test3
 
 
 sleep 10
