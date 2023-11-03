89364bbc6eebf42350793465d6b2df508998efe6
Alonso_Carrasco2000 <alonso.carrasco@edutecno.com>
2023-11-02 21:03:26 -0300

    thing

--- alonc_slurm/AB5_exp_num.sh
+++ alonc_slurm/AB5_exp_num.sh
@@ -2,16 +2,14 @@
 #SBATCH --job-name=hpc_t1_klempau       # nombre del trabajo
 #SBATCH -p ialab-high
 #SBATCH -t 0-2:00                    # tiempo maximo en el cluster (D-HH:MM)
-#SBATCH -o 1_task_16_cpu.out                 # STDOUT (A = )
+#SBATCH -o ex.out                 # STDOUT (A = )
 #SBATCH -e c_job.err                 # STDERR
 #SBATCH --mail-type=END,FAIL         # notificacion cuando el trabajo termine o fa$
 #SBATCH --mail-user=cristian.carrasco@uc.cl       # mail donde mandar las notifica$
 #SBATCH --workdir=/home/cristian.carrasco/T1-hpc         # direccion del d$
 #SBATCH --nodes 1                    # numero de nodos a usar
-#SBATCH --ntasks-per-node=1        # numero de trabajos (procesos) por nodo
+#SBATCH --ntasks-per-node=2        # numero de trabajos (procesos) por nodo
 #SBATCH --cpus-per-task=16          # numero de cpus (threads) por trabajo (proceso)
-
-
 
 echo '--------------------------- Test 4 --------------------------------'
 echo '1 tasks'
@@ -27,16 +25,7 @@ gcc -fopenmp -lm -O3 $file -lm -O3 -o $file_bin
 echo 'no exports'
 
 srun $file_bin -f test_files/test4
-
-srun $file_bin -f test_files/test4
-
-# for i in {1..16}; do
-#   export OMP_NUM_THREADS=$i
-#   echo 'export OMP_NUM_THREADS='$i
-#   srun $file_bin -f test_files/test4
-# done
-
-
+srun $file_bin -f test_files/test3
 
 
 
